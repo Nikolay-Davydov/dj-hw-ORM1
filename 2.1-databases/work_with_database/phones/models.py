997ef104725893722ba4ad1459f52e32cc8e40c4
Nikolay-Davydov <davidovns70@gmail.com>
2022-03-30 17:47:37 +0300

    add all files

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,19 @@
+from django.core.validators import MinValueValidator
 from django.db import models
-
+from slugify import slugify
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=30)
+    price = models.IntegerField(validators=[MinValueValidator(1)])
+    image = models.CharField(max_length=500)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(default=None)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
